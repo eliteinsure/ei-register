1e8cc9c024fdf796b572b00bad933eeaee4ae127
Samuel Magana <maganasamuel001@gmail.com>
2021-07-29 14:43:44 +1200

    Set brandmark colors to jetstream components

--- resources/views/vendor/jetstream/components/secondary-button.blade.php
+++ resources/views/vendor/jetstream/components/secondary-button.blade.php
@@ -1,3 +1,4 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition']) }}>
-    {{ $slot }}
+<button
+  {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-shark uppercase tracking-widest shadow-sm hover:text-tblue focus:outline-none focus:border-dsgreen focus:ring focus:ring-lmara active:text-lmara active:bg-gray-50 disabled:opacity-25 transition']) }}>
+  {{ $slot }}
 </button>
