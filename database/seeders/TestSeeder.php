b3db6adff199e8a8ddccb07ec9ab30d8648f0e60
Samuel Magana <maganasamuel001@gmail.com>
2021-07-30 10:46:33 +1200

    Created test seeder

---
+++ database/seeders/TestSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'email' => 'admin@mail.com',
+            'name' => 'Administrator',
+        ]);
+    }
+}
