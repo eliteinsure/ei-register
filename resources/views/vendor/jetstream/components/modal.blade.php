1563a16e88f5ebf2ff6e5a2d884bcdbf79ef056f
Samuel Magana <maganasamuel001@gmail.com>
2021-08-01 02:55:12 +1200

    Added tagify input to focusables

--- resources/views/vendor/jetstream/components/modal.blade.php
+++ resources/views/vendor/jetstream/components/modal.blade.php
@@ -22,7 +22,7 @@ $maxWidth = [
         show: @entangle($attributes->wire('model')).defer,
         focusables() {
             // All focusable element types...
-            let selector = 'a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])'
+            let selector = '.tagify__input, a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])'
 
             return [...$el.querySelectorAll(selector)]
                 // All non-disabled elements...
