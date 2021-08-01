4081e5a2f4e307e604cfe014e541816ea162d006
Samuel Magana <maganasamuel001@gmail.com>
2021-08-02 02:28:11 +1200

    Distinct mock adviser values

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Complaint;
 use App\Models\User;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class TestSeeder extends Seeder
 {
@@ -12,11 +15,30 @@ class TestSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         User::factory()->create([
             'email' => 'admin@mail.com',
             'name' => 'Administrator',
         ]);
+
+        $complainants = [];
+
+        $advisers = [];
+
+        foreach (range(1, 10) as $item) {
+            array_push($complainants, $faker->name());
+            array_push($advisers, $faker->name());
+        }
+
+        foreach (range(1, 100) as $item) {
+            $data = Complaint::factory()->make([
+                'complainant' => $faker->randomElement($complainants),
+            ])->toArray();
+
+            $data['tier'][1]['adviser'] = $faker->randomElement($advisers);
+
+            Complaint::create($data);
+        }
     }
 }
