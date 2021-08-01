9374ed79417d31c55b8cda5252449c534cfd4668
Samuel Magana <maganasamuel001@gmail.com>
2021-08-01 12:50:28 +1200

    Refactor look up text using alpine js

--- resources/views/components/lookup/text.blade.php
+++ resources/views/components/lookup/text.blade.php
@@ -1,65 +1,70 @@
 @props(['id', 'model'])
 
-{{-- @this.call: {{ $id }}LookupList --}}
-{{-- $this->dispatchBrowserEvent: {{ $id }}LookupList --}}
+{{-- @this.call: {{ $id }}LookupSearch --}}
+{{-- $this->dispatchBrowserEvent: {{ $id }}-lookup-list --}}
 
-<div wire:ignore>
+<div wire:ignore x-data="{{ $id }}LookupText"
+  x-on:{{ $id }}-lookup-list.window="lookupList">
   <x-jet-input id="{{ $id }}" type="hidden"
-    class="mt-1 block w-full lookup" />
+    class="mt-1 block w-full lookup" x-ref="input" />
 </div>
 
 @push('scripts')
   <script type="text/javascript">
-    window.onload = () => {
-      var input_{{ $id }} = document.querySelector('#{{ $id }}');
+    document.addEventListener('alpine:init', () => {
+      Alpine.data('{{ $id }}LookupText', () => ({
+        tagify: null,
+        init() {
+          window.onload = () => {
+            this.tagify = new window.Tagify(this.$refs.input, {
+              dropdown: {
+                enabled: 1,
+                mapValueTo: 'label',
+                searchKeys: [],
+              },
+              mode: 'select',
+              tagTextProp: 'label',
+              callbacks: {
+                'focus': () => {
+                  this.lookupSearch('');
+                },
+                'input': _.debounce((event) => {
+                  this.lookupSearch(event.detail.value);
+                }, 250),
+                'change': (event) => {
+                  var value = event.detail.value;
 
-      var tagify_{{ $id }} = new Tagify(input_{{ $id }}, {
-        dropdown: {
-          enabled: 1,
-          mapValueTo: 'label',
-          searchKeys: [],
+                  if (value) {
+                    var data = JSON.parse(value);
+
+                    this.$wire.set('{{ $model }}', data[0].value);
+                  } else {
+                    this.$wire.set('{{ $model }}', null);
+                  }
+                }
+              }
+            });
+          }
         },
-        mode: 'select',
-        tagTextProp: 'label',
-      });
+        lookupSearch(search) {
+          var controller;
 
-      var controller;
+          this.tagify.whitelist = null;
 
-      const tagify_{{ $id }}LookupList = (search) => {
-        tagify_{{ $id }}.whitelist = null;
+          controller && conroller.abort();
 
-        controller && controller.abort();
+          controller = new AbortController();
 
-        controller = new AbortController();
+          this.tagify.loading(true).dropdown.hide();
 
-        tagify_{{ $id }}.loading(true).dropdown.hide();
+          this.$wire.{{ $id }}LookupSearch(search);
+        },
+        lookupList(event) {
+          this.tagify.whitelist = event.detail;
 
-        @this.call('{{ $id }}LookupList', search);
-      }
-
-      tagify_{{ $id }}.on('focus', (event) => {
-        tagify_{{ $id }}LookupList('');
-      });
-
-      tagify_{{ $id }}.on('input', _.debounce((event) => {
-        tagify_{{ $id }}LookupList(event.detail.value);
-      }, 250));
-
-      window.addEventListener('{{ $id }}LookupList', (event) => {
-        tagify_{{ $id }}.whitelist = event.detail;
-
-        tagify_{{ $id }}.loading(false).dropdown.show();
-      });
-
-      tagify_{{ $id }}.on('change', (event) => {
-        if (event.detail.value) {
-          var data = JSON.parse(event.detail.value);
-
-          @this.set('{{ $model }}', data[0].value);
-        } else {
-          @this.set('{{ $model }}', null);
+          this.tagify.loading(false).dropdown.show();
         }
-      });
-    }
+      }));
+    });
   </script>
 @endpush
