270cbbe775aeb4615fd53351377ae2d98dbbc385
Samuel Magana <maganasamuel001@gmail.com>
2021-08-04 22:22:54 +1200

    Refactor date picker blade component

--- resources/views/components/date-picker.blade.php
+++ resources/views/components/date-picker.blade.php
@@ -6,25 +6,17 @@
     ],
 ])
 
-@php
-$id = 'Flatpickr_' . $attributes->get('id', md5('flatpickr'));
-@endphp
+<div wire:ignore x-data="{
+    value: @entangle($attributes->wire('model')),
+    instance: undefined
+  }"
+  x-init="
+    instance = flatpickr($refs.input, {{ json_encode($options) }});
 
-<div wire:ignore x-data="{{ $id }}">
-  <x-jet-input
-    {{ $attributes->merge(['type' => 'text', 'class' => 'block w-full mt-1', 'x-ref' => 'input']) }} />
-</div>
-
-@push('scripts')
-  <script type="text/javascript">
-    document.addEventListener('alpine:init', () => {
-      Alpine.data('{{ $id }}', () => ({
-        init() {
-          document.addEventListener('DOMContentLoaded', () => {
-            flatpickr(this.$refs.input, @json($options));
-          });
-        }
-      }));
+    $watch('value', value => {
+      instance.setDate(value, true);
     });
-  </script>
-@endpush
+  ">
+  <x-jet-input x-ref="input" x-bind:value="value"
+    {{ $attributes->merge(['type' => 'text', 'class' => 'block w-full mt-1']) }} />
+</div>
