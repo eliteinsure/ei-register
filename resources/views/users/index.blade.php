06dafb24364e1afd3871ab355525796df6376747
Samuel Magana <maganasamuel001@gmail.com>
2021-08-10 13:27:49 +1200

    Created route, controller, and blade view for users

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+  @section('title', 'Users')
+
+  <x-slot name="header">
+    <x-page-header>
+      Users
+    </x-page-header>
+  </x-slot>
+
+  <x-page-wrap>
+    livewire components here...
+  </x-page-wrap>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\PdfController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +26,10 @@ Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
     Route::resource('complaints', ComplaintController::class)->only([
         'index',
     ]);
+
+    Route::resource('users', UserController::class)->only([
+        'index',
+    ])->middleware('role:admin');
 
     Route::group(['as' => 'pdf.', 'prefix' => 'pdf', 'url' => 'pdf'], function () {
         Route::get('complaint', [PdfController::class, 'complaint'])->name('complaint');
