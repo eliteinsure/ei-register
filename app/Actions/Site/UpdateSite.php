4e17665e97b35b335331a46d8a3290956529a925
Samuel Magana <maganasamuel001@gmail.com>
2021-08-17 16:50:40 +1200

    Created actions for softwares

---
+++ app/Actions/Site/UpdateSite.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Actions\Site;
+
+use App\Models\Site;
+use App\Traits\Validators\SiteValidator;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class UpdateSite
+{
+    use SiteValidator;
+
+    public function update($input, Site $site)
+    {
+        $rules = $this->siteRules();
+
+        $rules['name'] = ['required', 'string', Rule::unique('sites')->ignore($site)];
+        $rules['url'] = ['required', 'string', Rule::unique('sites')->ignore($site)];
+
+        $data = Validator::make($input, $rules, [], $this->siteAttributes())->validate();
+
+        if (! $data['update_date']) {
+            $data['update_date'] = null;
+        }
+
+        $site->update($data);
+
+        return $site;
+    }
+}
