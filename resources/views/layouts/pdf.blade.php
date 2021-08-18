9cd060df43fdf410494f2158e8c3d8e6869c69c7
Samuel Magana <maganasamuel001@gmail.com>
2021-08-18 23:54:56 +1200

    Created layout for pdf

---
+++ resources/views/layouts/pdf.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{ $title }}</title>
+
+  <link rel="stylesheet" href="{{ mix('css/pdf.css') }}" />
+</head>
+
+<body>
+  <htmlpageheader name="page-header-first">
+    <table class="header">
+      <tr>
+        <td class="header-left-box">
+          &nbsp;
+        </td>
+        <td class="header-image"><img
+            src="{{ asset('images/logo-only.png') }}"
+            height="0.76in" /></td>
+        <td class="header-title">{{ $title }}</td>
+        <td class="header-right-box">
+          &nbsp;
+        </td>
+      </tr>
+    </table>
+  </htmlpageheader>
+
+  <htmlpageheader name="page-header">
+    <table class="header">
+      <tr>
+        <td class="header-left-box">
+          &nbsp;
+        </td>
+        <td class="header-image"><img
+            src="{{ asset('images/logo-only.png') }}"
+            height="0.76in" /></td>
+        <td class="header-title">&nbsp;</td>
+        <td class="header-right-box">
+          &nbsp;
+        </td>
+      </tr>
+    </table>
+  </htmlpageheader>
+
+  <htmlpagefooter name="page-footer">
+    <table class="table-footer">
+      <tr>
+        <td class="footer-logo">
+          <img src="{{ asset('images/horizontal-logo.png') }}"
+            width="2.12in" />
+        </td>
+        <td class="footer-page">
+          <a
+            href="{{ config('services.company.url') }}"
+            class="footer-link"
+            target="_blank">{{ config('services.company.web') }}</a>&nbsp;|&nbsp;Page
+          {PAGENO}
+        </td>
+      </tr>
+    </table>
+  </htmlpagefooter>
+
+  <div class="margin">
+    @yield('content')
+  </div>
+</body>
+
+</html>
