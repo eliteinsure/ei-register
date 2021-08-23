423ae503c53f4ee716e9839677cc06944f7c2796
Samuel Magana <maganasamuel001@gmail.com>
2021-08-23 14:28:48 +1200

    Created validator for claims

---
+++ app/Traits/Validators/ClaimValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits\Validators;
+
+trait ClaimValidator
+{
+    public function claimRules()
+    {
+        return [
+            'client_name' => ['required', 'string', 'max:255'],
+            'insurer' => ['required', 'in:' . implode(',', config('services.complaint.insurers'))],
+            'policy_number' => ['required', 'string', 'max:255'],
+            'nature' => ['required', 'in:' . implode(',', config('services.claim.natures'))],
+            'type' => ['required', 'in:' . implode(',', config('services.claim.types'))],
+            'status' => ['required', 'in:' . implode(',', config('services.claim.status'))],
+        ];
+    }
+
+    public function claimAttributes()
+    {
+        return [
+            'client_name' => 'Client Name',
+            'insurer' => 'Insurer',
+            'policy_number' => 'Policy Number',
+            'nature' => 'Nature',
+            'type' => 'Type',
+            'status' => 'Status',
+        ];
+    }
+}
