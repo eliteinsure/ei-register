43f177e8d4838f433d1066474c7e875bb7bdb97c
Samuel Magana <maganasamuel001@gmail.com>
2021-08-24 16:02:04 +1200

    Installed filepond and filepond plugins

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,3 +9,9 @@ window.Tagify = Tagify;
 
 import flatpickr from 'flatpickr';
 window.flatpickr = flatpickr;
+
+import * as FilePond from 'filepond';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+window.FilePond = FilePond;
+FilePond.registerPlugin(FilePondPluginFileValidateType, FilePondPluginFileValidateSize);
