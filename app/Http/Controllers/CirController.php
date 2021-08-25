e164cb17b607501dd4edcbc82b2b9a89a69770cc
Samuel Magana <maganasamuel001@gmail.com>
2021-08-26 11:14:46 +1200

    Create url for logging in to cir site

---
+++ app/Http/Controllers/CirController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CirController extends Controller
+{
+    public function login()
+    {
+        $token = auth()->user()->tokens()->first();
+
+        if (! $token) {
+            $token = auth()->user()->createToken('cir');
+        }
+
+        $token = auth()->user()->tokens()->first();
+
+        return redirect()->away(config('services.cir.url') . '/login/' . $token->token);
+    }
+}
